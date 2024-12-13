da8757dcf0ab21ca3a1291657a08a5a1a0b4032d
gidragir <ilyabalter1@gmail.com>
2024-12-14 01:24:06 +0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import purgecss from "astro-purgecss";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  base: "/resume_devops/",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    purgecss({
+      variables: true,
+      keyframes: false,
+      safelist: {
+        greedy: [
+          /*astro*/
+        ],
+      },
+      content: [process.cwd() + "/src/**/*.{astro,jsx,tsx,css,scss}"],
+    }),
+    compress({
+      Image: false,
+      SVG: false,
+    }),
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+      },
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "bg-default": "var(--default-background)",
+      },
+    },
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "mocha",
+    }),
+  ],
+};
